cc1e1fb4f578558acaa31c5eb69e46eb930e7bb5
Abhishek <neoscreenager@gmail.com>
2019-10-02 14:41:24 +0530

    Making predictions using MNIST dataset images

--- mnist_client.py
+++ mnist_client.py
@@ -1,44 +1,46 @@
+'''
+    Client code to test the mnist model served via tenorflow serving.
+    It takes the test image from mnist dataset and call REST api for predictions.
+    TO DO: TO MAKE A PREDICTION USING A FILE FROM THE DISK INSTEAD OF PICKING FROM DATASET.
+'''
 import warnings
 warnings.filterwarnings('ignore',category=FutureWarning) # to ignore the numpy warnings
 import json
 import numpy as np
 import requests
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
 
-def load_image(filename):
-	# load the image
-	img = load_img(filename, color_mode="grayscale", target_size=(28, 28))
-	# convert to array
-	img = img_to_array(img)
-	# reshape into a single sample with 1 channel
-	img = img.reshape(28, 28)
-	# prepare pixel data
-	img = img.astype('float32')
-	img = img / 255.0
-	return img
+# loading test images from mnist dataset and randomly choosing one and call serving
+# to get predictions
+# loading traing data and test data from mnist dataset
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# Preprocessing our input image
-img = load_image("./digit_7.png")
-# print(img.tolist())
-# headers = {"content-type": "application/json"}
-# data = json.dumps({"signature_name": "serving_default", "instances": [{'input_image': img.tolist()}]})
-#data = json.dumps({"instances": [{'input_image': img.tolist()}]})
-#data = json.dumps({"instances": img.tolist()})
-# data = {'instances': [{'input_image': img.tolist()}]}
-payload = {
-    "instances": [{'input_image': img.tolist()}]
-}
+# preprocessing the image data
+train_images = train_images.reshape((60000,28 * 28))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000,28 * 28))
+test_images = test_images.astype('float32') / 255
+
+# preprocessing labels by catagorically encoding them
+train_labels = to_categorical(train_labels)
+test_labels  = to_categorical(test_labels)
+
+def show(idx, title):
+  plt.figure()
+  plt.imshow(test_images[idx].reshape(28,28))
+  plt.axis('off')
+  plt.title('\n\n{}'.format(title), fontdict={'size': 16})
+  plt.show()
 
 # sending post request to TensorFlow Serving server
-json_response = requests.post('http://localhost:9000/v1/models/MnistClassifier:predict', json=payload) #, headers=headers)
-print(json_response)
+data = json.dumps({"signature_name": "serving_default", "instances": test_images[0:3].tolist()})
+headers = {"content-type": "application/json"}
+json_response = requests.post('http://localhost:9000/v1/models/MnistClassifier:predict', data=data, headers=headers)
 predictions = json.loads(json_response.text)['predictions']
 print(predictions)
-#pred = json.loads(r.content.decode('utf-8'))
-#print(pred)
-# Decoding the response
-# decode_predictions(preds, top=5) by default gives top 5 results
-# You can pass "top=10" to get top 10 predicitons
-# print(json.dumps(inception_v3.decode_predictions(np.array(pred['predictions']))[0]))
-#print(json.dumps(np.array(pred['predictions'])[0]))
+for i in range(0,3):
+    show(i, 'The model thought this was a {} (class {}), and it was actually a {} (class {})'.format(
+  np.argmax(predictions[i]), test_labels[i], np.argmax(predictions[i]), test_labels[i]))
